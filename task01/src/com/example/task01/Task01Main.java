26ebb738ad5ad9bf32ef45e5b219a4751de8f5f1
Shinen <Fixmaks99@gmail.com>
2020-12-22 10:35:40 +0500

    task01[Ефимов]

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -15,7 +15,15 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+        int checkSum = 0;
+        int stream_byte = inputStream.read();
+        while (stream_byte != -1) {
+            checkSum = Integer.rotateLeft(checkSum, 1) ^ stream_byte;
+            stream_byte = inputStream.read();
+        }
+        return checkSum;
     }
 }
